290a1b1c07b53ca514178d84a6d7c911366d1292
JustEvil <thatjustevil@gmail.com>
2024-05-19 13:54:08 -0600

    unnecessary throw

--- seyfert.config.js
+++ seyfert.config.js
@@ -4,12 +4,9 @@ import { GatewayIntentBits } from "discord-api-types/v10";
 import { config } from "seyfert";
 
 import { DEBUG_MODE } from "#stelle/data/Constants.js";
-import { InvalidEnvironment } from "#stelle/errors";
-
-if (!process.env.TOKEN) throw new InvalidEnvironment("'TOKEN' in '.env' file cannot be empty or undefined.");
 
 export default config.bot({
-    token: process.env.TOKEN,
+    token: process.env.TOKEN ?? "Trailblazer",
     debug: DEBUG_MODE,
     intents: [
         GatewayIntentBits.Guilds,
