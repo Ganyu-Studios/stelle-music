3f59319872a60d2cd52944d0b17c37f288cd54b0
JustEvil <thatjustevil@gmail.com>
2024-05-26 19:21:11 -0600

    now, use pnpm as package manager

--- scripts/clean.js
+++ scripts/clean.js
@@ -1,32 +1,17 @@
 //@ts-check
 
-import { exec } from "node:child_process";
 import { rm } from "node:fs/promises";
-import { promisify } from "node:util";
 import { existsSync } from "node:fs";
 import { resolve } from "node:path";
 
-import chalk from "chalk";
-
-const execSync = promisify(exec);
-
 (async () => {
-    console.info("Attemping to compile...");
-
-    const timeStart = Date.now();
+    console.info("Deleting files...");
 
     try {
         const path = resolve("dist");
-        const exist = existsSync(path);
-
-        if (exist) await rm(path, { recursive: true });
-
-        await execSync("pnpm build");
-
-        console.info(`Done! Compiled at: ${Date.now() - timeStart}ms`);
-    } catch (error) {
-        console.info("Error! Compilation error!\n")
-        console.info(chalk.red(error.stdout));
+        if (existsSync(path)) await rm(path, { recursive: true });
+        console.info("Done! Cleared.");
+    } catch (_) {
         process.exit(1);
     };
 })();
