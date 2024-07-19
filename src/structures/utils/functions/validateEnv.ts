ec1023e6d2dfeb8459e76f7f6b9fe16d6df6efd5
JustEvil <thatjustevil@gmail.com>
2024-07-18 18:46:11 -0600

    i forgot this

--- src/structures/utils/functions/validateEnv.ts
+++ src/structures/utils/functions/validateEnv.ts
@@ -14,9 +14,6 @@ export function validateEnv() {
     logger.info("Validating '.env' file variables...");
 
     if (!process.env.TOKEN) throw new InvalidEnvironment("The variable: 'TOKEN' in the '.env' cannot be empty or undefined.");
-    if (!process.env.SPOTIFY_ID) throw new InvalidEnvironment("The variable: 'SPOTIFY_ID' in the '.env' cannot be empty or undefined.");
-    if (!process.env.SPOTIFY_SECRET)
-        throw new InvalidEnvironment("The variable: 'SPOTIFY_SECRET' in the '.env' cannot be empty or undefined.");
 
     return logger.info("I'm not able to found missing variables.");
 }
