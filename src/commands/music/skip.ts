db1f829fe322d7478908f3a7e660f02c6b3e2db3
JustEvil <thatjustevil@gmail.com>
2024-08-15 22:15:49 -0600

    test

--- src/commands/music/skip.ts
+++ src/commands/music/skip.ts
@@ -18,7 +18,7 @@ const options = {
     contexts: ["Guild"],
     aliases: ["sk"],
 })
-@StelleOptions({ cooldown: 5, checkPlayer: true, inVoice: true, sameVoice: true, checkNodes: true })
+@StelleOptions({ cooldown: 5, checkPlayer: true, inVoice: true, sameVoice: true, checkNodes: true, checkQueue: true })
 @Options(options)
 @LocalesT("locales.skip.name", "locales.skip.description")
 export default class SkipCommand extends Command {
