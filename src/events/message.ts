a23ad553d732ac6a8d5fd9349d9343ec741fa2f8
JustEvil <71156616+EvilG-MC@users.noreply.github.com>
2025-03-30 18:06:38 -0600

    doing a refactor, yay

--- src/events/message.ts
+++
@@ -1,9 +0,0 @@
-import { createEvent } from "seyfert";
-import { mentionListener } from "#stelle/listeners";
-
-export default createEvent({
-    data: { name: "messageCreate" },
-    async run(message, client): Promise<void> {
-        await mentionListener(client, message);
-    },
-});
--- src/structures/listeners/index.ts
+++
@@ -1,2 +0,0 @@
-export { mentionListener } from "./client/mentionListener.js";
-export { playerListener } from "./client/playerListener.js";
