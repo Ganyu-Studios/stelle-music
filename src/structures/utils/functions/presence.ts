dc4e7fd39aadbccf7b29672dd4a6caee4ff51980
JustEvil <71156616+EvilG-MC@users.noreply.github.com>
2025-04-03 20:42:20 -0600

    time formatter

--- src/structures/utils/functions/presence.ts
+++ src/structures/utils/functions/presence.ts
@@ -2,6 +2,7 @@ import type { Guild, UsingClient } from "seyfert";
 import { type GatewayActivityUpdateData, PresenceUpdateStatus } from "seyfert/lib/types/index.js";
 
 import { Constants } from "#stelle/utils/data/constants.js";
+import { ms } from "#stelle/utils/functions/time.js";
 
 /**
  *
@@ -30,7 +31,7 @@ export function changePresence(client: UsingClient): void {
             afk: false,
             since: Date.now(),
         });
-    }, 35e3);
+    }, ms("25s"));
 
     client.gateway.setPresence({
         status: PresenceUpdateStatus.Online,
